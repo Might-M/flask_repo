b67da4df6c0956d99f05fdb957cc2b40047b91b0
Might-M <104938037@qq.com>
2016-09-30 17:07:16 +0800

    errors handler

---
+++ app/main/error.py
@@ -0,0 +1,10 @@
+from flask import renfer_template
+from . import main
+
+@main.app_errorhandler(404)
+def page_not_found(e):
+	return(renfer_template('404.html'), 404)
+
+@main.app_errorhandler(500)
+def internal_server_error(e):
+	return(renfer_template('500.html'), 500)
